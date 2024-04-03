8760abd76349c8236f88c289470f1a5dada998c4
bromles <all.bromles@gmail.com>
2024-04-03 20:11:59 +0300

    update c++

--- hello-world/Cpp/drogon/controllers/HelloCtrl.h
+++ hello-world/Cpp/drogon/controllers/HelloCtrl.h
@@ -4,14 +4,13 @@
 
 using namespace drogon;
 
-class HelloCtrl : public drogon::HttpSimpleController<HelloCtrl> {
-public:
-    void
-    asyncHandleHttpRequest(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) override;
-
+class HelloCtrl : public drogon::HttpSimpleController<HelloCtrl>
+{
+  public:
+    void asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) override;
     PATH_LIST_BEGIN
-        // list path definitions here;
-        // PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
-        PATH_ADD("/", Get);
+    // list path definitions here;
+    // PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
+    PATH_ADD("/");
     PATH_LIST_END
 };
